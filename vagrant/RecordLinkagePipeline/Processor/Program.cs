d44e1b8b5893bf021120acdddf3ffe3189e367ac
David Forshner <DForshner@gmail.com>
2016-05-02 19:06:10 -0400

    Added kludge to get mono directories working

--- vagrant/RecordLinkagePipeline/Processor/Program.cs
+++ vagrant/RecordLinkagePipeline/Processor/Program.cs
@@ -1,16 +1,39 @@
-﻿using Pipeline;
+﻿using System;
+using System.IO;
+using Pipeline;
 using Pipeline.Extraction;
 
 namespace Processor
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var products = new IngestProductFile().Ingest("../../../Resources/products.txt");
-            var listings = new IngestListingFile().Ingest("../../../Resources/listings.txt", 500);
+            // TODO: Find some cleaner way of doing this.
+            if (!IsMono())
+            {
+                Directory.SetCurrentDirectory("../../../");
+            }
 
-            var simple = new SimplePipeline().FindMatches(products, listings);
+            var products = new IngestProductFile().Ingest("./Resources/products.txt");
+            var listings = new IngestListingFile().Ingest("./Resources/listings.txt", 500);
+
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            Console.WriteLine(Environment.CurrentDirectory);
+
+            foreach(var prod in products)
+            {
+                Console.WriteLine("{0} {1}", prod.Manufacturer, prod.Model);
+            }
+            foreach(var listing in listings)
+            {
+                Console.WriteLine("{0} {1}", listing.Manufacturer, listing.Title);
+            }
+        }
+
+        public static bool IsMono()
+        {
+            return (Type.GetType("Mono.Runtime") != null);
         }
     }
 }
