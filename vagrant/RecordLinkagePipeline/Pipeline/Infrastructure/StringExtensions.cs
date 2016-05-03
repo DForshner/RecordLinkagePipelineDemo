b857f3541925b024cc4b09c2693b93ef5fb745e0
David Forshner <DForshner@gmail.com>
2016-05-03 18:58:53 -0400

    Added string extensions

--- vagrant/RecordLinkagePipeline/Pipeline/Infrastructure/StringExtensions.cs
+++ vagrant/RecordLinkagePipeline/Pipeline/Infrastructure/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Pipeline.Infrastructure
 {
@@ -11,6 +12,48 @@ namespace Pipeline.Infrastructure
         public static string[] TokenizeOnWhiteSpace(this string str)
         {
             return str.Split(null); // null splits based on Unicode Char.IsWhiteSpace
+        }
+
+        /// <summary>
+        /// Generates character n-grams
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<string> CreateBiTriQuadCharacterNGrams(this string str)
+        {
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                yield return new string(new char[] { str[i], str[i + 1] });
+            }
+
+            for (var i = 0; i < str.Length - 2; i++)
+            {
+                yield return new string(new char[] { str[i], str[i + 1], str[i + 2] });
+            }
+
+            for (var i = 0; i < str.Length - 3; i++)
+            {
+                yield return new string(new char[] { str[i], str[i + 1], str[i + 2], str[i + 3] });
+            }
+        }
+
+        /// <summary>
+        /// Generates word/token shingles
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<string> CreateUniBiTokenShingles(this string str)
+        {
+            var tokens = str.Split(null); // null splits based on Unicode Char.IsWhiteSpace
+
+            for (var i = 0; i < tokens.Length - 1; i++)
+            {
+                yield return tokens[i];
+                yield return tokens[i] + tokens[i + 1];
+            }
+
+            if (tokens.Length > 0)
+            {
+                yield return tokens[tokens.Length - 1];
+            }
         }
     }
 }
---
+++ vagrant/RecordLinkagePipeline/PipelineTests/Infrastructure/StringExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pipeline.Infrastructure;
+
+namespace Pipeline.UnitTests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void ExpectBiTriQuadCharacterNGramsFromManufacturerName()
+        {
+            var results = "abcd".CreateBiTriQuadCharacterNGrams();
+            Assert.IsTrue(results.OrderBy(x => x).AsQueryable().SequenceEqual(new[] { "ab", "abc", "abcd", "bc", "bcd", "cd" }.AsQueryable()));
+        }
+
+        [TestMethod]
+        public void ExpectUniBiShinglesFromModelNames()
+        {
+            var results = "ant bee cat".CreateUniBiTokenShingles();
+            Assert.IsTrue(results.OrderBy(x => x).AsQueryable().SequenceEqual(new[] { "ant", "antbee", "bee", "beecat", "cat" }.AsQueryable()));
+        }
+    }
+}
