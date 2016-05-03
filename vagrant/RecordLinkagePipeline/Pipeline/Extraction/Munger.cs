928876d31e35ae38d5cf19f591e7a3c37fa48d88
David Forshner <DForshner@gmail.com>
2016-05-03 18:58:21 -0400

    Added filed munging

---
+++ vagrant/RecordLinkagePipeline/Pipeline/Extraction/Munger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Pipeline.Shared;
+
+namespace Pipeline.Extraction
+{
+    internal static class Munger
+    {
+        /// <summary>
+        /// Remove punctuation, multiple spaces in a row, line endings, and lowercase everything
+        /// </summary>
+        public static string Munge(string original)
+        {
+            var temp = original.ToCharArray();
+            var result = new List<char>(original.Length);
+            bool lastCharWasSpace = false;
+            foreach (var c in temp)
+            {
+                if (char.IsLetter(c) || char.IsNumber(c))
+                {
+                    result.Add(char.ToLower(c));
+                    lastCharWasSpace = false;
+                }
+                else if (!lastCharWasSpace)
+                {
+                    result.Add(' ');
+                    lastCharWasSpace = true;
+                }
+            }
+            return new String(result.ToArray());
+        }
+    }
+}
---
+++ vagrant/RecordLinkagePipeline/PipelineTests/Extraction/MungerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pipeline;
+using Pipeline.Extraction;
+
+namespace Pipeline.UnitTests.Extraction
+{
+    [TestClass]
+    public class MungerTests
+    {
+        [TestMethod]
+        public void WhenAllValid_ExpectNoChange()
+        {
+            var expected = Munger.Munge("1 This is a test 1");
+            var result = Munger.Munge("1 This is a test 1");
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ExpectNonAlphaOrNumericCharRemoved()
+        {
+            var expected = Munger.Munge("1 This is a test 1 ");
+            var result = Munger.Munge("1. This=is-a test 1!!!");
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
