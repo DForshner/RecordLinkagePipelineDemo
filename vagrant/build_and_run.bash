d1086620ede64863bad48fc5a295517d9ac06fab
David Forshner <DForshner@gmail.com>
2016-05-06 17:09:21 -0400

    Fixed path

--- vagrant/build_and_run.bash
+++ vagrant/build_and_run.bash
@@ -6,7 +6,7 @@ set -e
 echo "\e[92m --- build_and_run.bash - Start --- \e[0m"
 
 echo -e "\e[32m ----------------------------------------------- Change to solution dir \e[0m"
-cd RecordLinkagePipeline
+cd /vagrant/vagrant/RecordLinkagePipeline
 echo pwd
 
 echo -e "\e[32m ----------------------------------------------- Compile  \e[0m"
