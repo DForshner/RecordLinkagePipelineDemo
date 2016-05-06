1f86bca7a63c9e1ee3d81f8354170e84f7e4d655
David Forshner <DForshner@gmail.com>
2016-05-06 17:53:23 -0400

    Added helper

---
+++ vagrant/RecordLinkagePipeline/Pipeline/Infrastructure/FloatExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipeline.Infrastructure
+{
+    public static class FloatExtensions
+    {
+        public static bool IsNearZero(this float v, float epsilon = 0.00001F)
+        {
+            return v > -epsilon && v < epsilon;
+        }
+    }
+}
