2e7f44b9f02bea88a5a9f3a00aa535f67b4235de
David Forshner <DForshner@gmail.com>
2016-05-10 17:22:07 -0400

    Cleanup

--- vagrant/RecordLinkagePipeline/Pipeline/Shared/ExchangeRate.cs
+++ vagrant/RecordLinkagePipeline/Pipeline/Shared/ExchangeRate.cs
@@ -4,7 +4,9 @@ namespace Pipeline.Shared
     public class ExchangeRate
     {
         public string SourceCurrencyCode { get; set; }
+
         public string DestinationCurrencyCode { get; set; }
+
         public decimal Rate { get; set; }
     }
 }
