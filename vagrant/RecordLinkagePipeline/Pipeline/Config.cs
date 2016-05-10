b7d789a4ac713a5e5792e4ce473fa39dc418a7b9
David Forshner <DForshner@gmail.com>
2016-05-10 17:21:28 -0400

    Configure via config.txt file

---
+++ vagrant/RecordLinkagePipeline/Pipeline/Config.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Pipeline
+{
+    public class Config
+    {
+        // Alias generation
+        public int ManufacturerNameCutoff { get; set; }
+        public float PossibleAliasPercentile { get; set; }
+        public float CommonWordPercentile { get; set; }
+
+        // Naive Bayes classifier
+        public int MinNumWords { get; set; }
+        public float WordRatio { get; set; }
+
+        // Heuristic classifier
+        public decimal LowPriceCutoff { get; set; }
+        public decimal HighPriceCutoff { get; set; }
+        public float Threshold { get; set; }
+
+        // Price outlier classifier
+        public decimal LowerRangeMultiplier { get; set; }
+        public decimal UpperRangeMultiplier { get; set; }
+    }
+}
---
+++ vagrant/RecordLinkagePipeline/Pipeline/Extraction/ConfigParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Pipeline.Shared;
+
+namespace Pipeline.Extraction
+{
+    internal static class ConfigParser
+    {
+        public static Config Parse(String str)
+        {
+            Debug.Assert(!String.IsNullOrEmpty(str));
+
+            var jsonObj = Newtonsoft.Json.Linq.JObject.Parse(str);
+            return new Config
+            {
+                ManufacturerNameCutoff = int.Parse((string)jsonObj["manufacturer_name_cutoff"]),
+                PossibleAliasPercentile = float.Parse((string)jsonObj["possible_alias_percentile"]),
+                CommonWordPercentile = float.Parse((string)jsonObj["common_word_percentile"]),
+                MinNumWords = int.Parse((string)jsonObj["min_num_words"]),
+                WordRatio = float.Parse((string)jsonObj["word_ratio"]),
+                LowPriceCutoff = decimal.Parse((string)jsonObj["low_price_cutoff"]),
+                HighPriceCutoff = decimal.Parse((string)jsonObj["high_price_cutoff"]),
+                Threshold = float.Parse((string)jsonObj["threshold"]),
+                LowerRangeMultiplier = decimal.Parse((string)jsonObj["lower_range_multiplier"]),
+                UpperRangeMultiplier = decimal.Parse((string)jsonObj["upper_range_multiplier"]),
+            };
+        }
+    }
+}
