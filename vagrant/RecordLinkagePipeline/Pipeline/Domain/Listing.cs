92fe4c9f6853f0d944aa0fa872635056d401f6bc
David Forshner <DForshner@gmail.com>
2016-05-11 18:30:16 -0400

    Added comments

--- vagrant/RecordLinkagePipeline/Pipeline/Domain/Listing.cs
+++ vagrant/RecordLinkagePipeline/Pipeline/Domain/Listing.cs
@@ -4,10 +4,19 @@ namespace Pipeline.Domain
 {
     internal class Listing
     {
+        /// <summary>
+        /// Who manufactures the product for sale
+        /// </summary>
         public string Manufacturer { get; set; }
 
+        /// <summary>
+        /// Description of product for sale
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Currency code, e.g. USD, CAD, GBP, etc.
+        /// </summary>
         public string CurrencyCode { get; set; }
 
         public Decimal Price { get; set; }
--- vagrant/RecordLinkagePipeline/Pipeline/Domain/Product.cs
+++ vagrant/RecordLinkagePipeline/Pipeline/Domain/Product.cs
@@ -3,12 +3,18 @@ namespace Pipeline.Domain
 {
     internal class Product
     {
+        /// <summary>
+        /// A unique id for the product
+        /// </summary>
         public string Name { get; set; }
 
         public string Manufacturer { get; set; }
 
         public string Model { get; set; }
 
+        /// <summary>
+        /// Optional grouping of products
+        /// </summary>
         public string Family { get; set; }
 
         public string Original { get; set; }
