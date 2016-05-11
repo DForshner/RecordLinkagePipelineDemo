81dffa239abea724153f23bbc154750c37dd6d9e
David Forshner <DForshner@gmail.com>
2016-05-11 17:10:02 -0400

    Renamed

---
+++ vagrant/RecordLinkagePipeline/Pipeline/Analysis/TokenProbabilityCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Pipeline.Infrastructure;
+using Pipeline.Shared;
+
+namespace Pipeline.Analysis
+{
+    /// <summary>
+    /// Get term probability over all docs.
+    /// </summary>
+    internal static class TokenProbabilityCalculator
+    {
+        public static Dictionary<string, float> GetProbabilities<T>(IEnumerable<T> docs, Func<T, string> selector)
+        {
+            var freqByToken = new ConcurrentDictionary<string, int>();
+            var numDocs = 0;
+            docs.AsParallel()
+                .ForAll(x =>
+                {
+                    var tokens = selector(x).TokenizeOnWhiteSpace();
+                    foreach (var token in tokens)
+                    {
+                        freqByToken.AddOrUpdate(token, 1, (key, val) => { return val + 1; });
+                    }
+
+                    Interlocked.Increment(ref numDocs);
+                });
+
+            // Turn freq into probability per doc
+            return freqByToken.ToDictionary(x => x.Key, x => (float)x.Value / numDocs);
+        }
+    }
+}
--- vagrant/RecordLinkagePipeline/PipelineTests/Analysis/TokenProbabilityCalculatorTests.cs
+++ vagrant/RecordLinkagePipeline/PipelineTests/Analysis/TokenProbabilityCalculatorTests.cs
@@ -1,12 +1,13 @@
 ﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pipeline.Analysis;
+using Pipeline.Domain;
 using Pipeline.Shared;
 
 namespace Pipeline.UnitTests.Analysis
 {
     [TestClass]
-    public class TokenProbablityPerListingCalculatorTests
+    public class TokenProbabilityCalculatorTests
     {
         [TestMethod]
         public void ExpectTokenProbablityPerDoc()
@@ -21,7 +22,7 @@ namespace Pipeline.UnitTests.Analysis
                 new Listing { Title = "A B" },
                 new Listing { Title = "" },
             };
-            var result = TokenProbablityPerListingCalculator.GenerateTokenProbabilitiesPerListing(docs);
+            var result = TokenProbabilityCalculator.GetProbabilities(docs, x => x.Title);
 
             Assert.AreEqual(6F / 7F, result["A"], 0.01D);
             Assert.AreEqual(2F / 7F, result["F"], 0.01D);
