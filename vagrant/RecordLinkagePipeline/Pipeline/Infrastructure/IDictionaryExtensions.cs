151fc21e01ad259663da0eb65028e00e06e6273a
David Forshner <DForshner@gmail.com>
2016-05-12 16:38:56 -0400

    Added AddOrIncrement extension method

---
+++ vagrant/RecordLinkagePipeline/Pipeline/Infrastructure/IDictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pipeline.Infrastructure
+{
+    public static class IDictionaryTExtensions
+    {
+        /// <summary>
+        /// Add or increment the counter associated with the key.
+        /// </summary>
+        public static void AddOrIncrement<K>(this IDictionary<K, int> toUpdate, K key)
+        {
+            if (toUpdate.ContainsKey(key))
+                toUpdate[key]++;
+            else
+                toUpdate.Add(key, 1);
+        }
+    }
+}
